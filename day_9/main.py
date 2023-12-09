ef72167927ecc9f0b9561c3bfeafb3937b6ae41e
didudash <dianaruedade@gmail.com>
2023-12-09 12:30:18 +0100

    day_9 part 2

--- day_9/main.py
+++ day_9/main.py
@@ -2,16 +2,22 @@
 filepath = "puzzle_input.txt"
 
 
-def last_col_with_dif(lst):
-    result = [lst[-1]]
+def col_with_dif(lst, col):
+    result = [lst[col]]
     while any(lst) and len(lst) > 1:  # Until all elements are zero
         lst = [b - a for a, b in zip(lst, lst[1:])]
-        result.append(lst[-1])  # Save the last element
+        result.append(lst[col])  # Save the last element
     return result
 
 
 def rev_sum(lst):
     return sum(lst[::-1])  # In reverse order
+
+
+def rev_sub(lst):
+    for i in range(len(lst) - 1, 0, -1):
+        lst[i - 1] = lst[i - 1] - lst[i]
+    return lst[0] if lst else 0
 
 
 with open(filepath, "r") as file:
@@ -21,10 +27,20 @@ with open(filepath, "r") as file:
 def part_1():
     sum_hist = 0
     for lst in dataset:
-        last_cols = last_col_with_dif(lst)
+        last_cols = col_with_dif(lst, -1)
         hist = rev_sum(last_cols)
         sum_hist += hist
     print(sum_hist)
 
 
+def part_2():
+    sum_hist = 0
+    for lst in dataset:
+        last_cols = col_with_dif(lst, 0)
+        hist = rev_sub(last_cols)
+        sum_hist += hist
+    print(sum_hist)
+
+
 part_1()
+part_2()
