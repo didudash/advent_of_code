4b70c797e4e5bd6b9334fd2de873ef8d18cc0980
didudash <dianaruedade@gmail.com>
2023-12-11 18:27:45 +0100

    add day 11 part 2 and refactored

--- 11/main.py
+++ 11/main.py
@@ -1,4 +1,3 @@
-import math
 import numpy as np
 from itertools import combinations
 
@@ -10,30 +9,55 @@ with open(filepath, "r") as file:
 
 data = [list(line.strip()) for line in lines]
 dataset = np.array(data)
-
 char = "#"
 
-rows_wo_char = np.where(~np.any(dataset == char, axis=1))[0]
-cols_wo_char = np.where(~np.any(dataset == char, axis=0))[0]
-print(rows_wo_char)
-print(cols_wo_char)
+
+def rows_cols_without_char(dataset, char):
+    rows_wo_char = np.where(~np.any(dataset == char, axis=1))[0]
+    cols_wo_char = np.where(~np.any(dataset == char, axis=0))[0]
+    return rows_wo_char, cols_wo_char
 
 
-# does it make sense appending them?
-def part_1():
-    # Appending new rows
-    dataset = np.insert(dataset, rows_wo_char, ".", axis=0)
-    # Appending new columns
-    dataset = np.insert(dataset, cols_wo_char, ".", axis=1)
-
+def pos_with_char(dataset, char):
     positions = np.where(dataset == char)
     pos_pairs = list(zip(positions[0], positions[1]))
+    return pos_pairs
 
+
+def add_comb_paths(pos_pairs):
     sum_short_path = 0
-
     for pair1, pair2 in combinations(pos_pairs, 2):
         short_path = abs(pair1[0] - pair2[0]) + abs(pair1[1] - pair2[1])
-        if short_path != 0:
-            sum_short_path += short_path
-
+        sum_short_path += short_path
     print(sum_short_path)
+
+
+def add_cumulative_offset(rows, cols, positions, offset):
+    new_positions = []
+    for pos in positions:
+        row, col = pos
+        row_offset = sum(row > r for r in rows) * offset
+        row += row_offset
+        col_offset = sum(col > c for c in cols) * offset
+        col += col_offset
+        new_positions.append((row, col))
+    return new_positions
+
+
+def sum_of_paths(offset):
+    rows_wo_char, cols_wo_char = rows_cols_without_char(dataset, char)
+    pos_pairs = pos_with_char(dataset, char)
+    new_positions = add_cumulative_offset(rows_wo_char, cols_wo_char, pos_pairs, offset)
+    add_comb_paths(new_positions)
+
+
+def part_1():
+    sum_of_paths(1)
+
+
+def part_2():
+    sum_of_paths(1_000_000 - 1)
+
+
+part_1()
+part_2()
