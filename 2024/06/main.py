752eff67342887b84532079bd1de3aacc4f6eda9
Diana Rueda <diana.rueda@studium.uni-hamburg.de>
2024-12-06 23:31:26 +0100

    Day 6 part 2

--- 2024/06/main.py
+++ 2024/06/main.py
@@ -1,3 +1,7 @@
+# Directions: Up, Right, Down, Left
+DIRECTIONS = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+
+
 def parse_txt_to_dict(file_path):
     """Parse the grid into a dictionary of (y, x): symbol."""
     with open(file_path, "r") as file:
@@ -18,16 +22,14 @@ def part_1(grid):
     if not start_pos:
         return 0
 
-    # Directions: Up, Right, Down, Left
-    directions = [(-1, 0), (0, 1), (1, 0), (0, -1)]
     current_dir = 0  # Start moving upward
     current_pos = start_pos
     unique_positions = set([current_pos])
 
     while True:
         next_pos = (
-            current_pos[0] + directions[current_dir][0],
-            current_pos[1] + directions[current_dir][1],
+            current_pos[0] + DIRECTIONS[current_dir][0],
+            current_pos[1] + DIRECTIONS[current_dir][1],
         )
 
         if next_pos not in grid:
@@ -44,7 +46,52 @@ def part_1(grid):
     print(len(unique_positions))
 
 
-# file_path = "toy_input.txt"
+def part_2(grid):
+    """
+    Simulates movement on the grid to detect loops.
+    """
+    start_pos = find_position_of_guard(grid)
+    if not start_pos:
+        return 0
+
+    loop_count = 0
+
+    for cell, value in grid.items():
+        if value == "#":
+            continue
+
+        current_pos = start_pos
+        current_dir = 0
+        visited_states = {}
+        grid[cell] = "#"  # Temporarily mark the cell as an obstacle
+
+        while current_pos in grid:
+            if grid[current_pos] == "#":
+                # Step back and turn 90 degrees to the right
+                current_pos = (
+                    current_pos[0] - DIRECTIONS[current_dir][0],
+                    current_pos[1] - DIRECTIONS[current_dir][1],
+                )
+                current_dir = (current_dir + 1) % 4
+                continue
+
+            state = (current_pos, current_dir)
+            if state in visited_states:
+                loop_count += 1
+                break
+
+            visited_states[state] = True  # Mark the state as visited
+            current_pos = (
+                current_pos[0] + DIRECTIONS[current_dir][0],
+                current_pos[1] + DIRECTIONS[current_dir][1],
+            )
+
+        grid[cell] = "."  # Restore the original cell value
+
+    print(loop_count)
+
+
 file_path = "puzzle_input.txt"
 grid = parse_txt_to_dict(file_path)
 part_1(grid)
+part_2(grid)
