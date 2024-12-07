eb0d749bc09c05ed675035404a395cf7136c1f22
Diana Rueda <diana.rueda@studium.uni-hamburg.de>
2024-12-07 12:29:43 +0100

    Day 7 part 2

--- 2024/07/main.py
+++ 2024/07/main.py
@@ -22,33 +22,46 @@ def read_and_parse_to_dict(file_path, split_char):
     return data_dict
 
 
-def evaluate_left_to_right(values, target):
+def evaluate_left_to_right(values, target, operators):
     if len(values) == 1:
         return values[0] == target
 
     num_operators = len(values) - 1
-    for operators in product(["+", "*"], repeat=num_operators):
+    for operators in product(operators, repeat=num_operators):
         result = values[0]
         for i, op in enumerate(operators):
             if op == "+":
                 result += values[i + 1]
             elif op == "*":
                 result *= values[i + 1]
+            elif op == "||":
+                result_str = str(result) + str(values[i + 1])
+                result = int(result_str)
         if result == target:
             return True
     return False
 
 
-def part_1(data_dict):
+def calculate_matching_sum(data_dict, operators):
     matching_test_values_sum = 0
     for key, all_values in data_dict.items():
         for values in all_values:
-            if evaluate_left_to_right(values, key):
+            if evaluate_left_to_right(values, key, operators):
                 matching_test_values_sum += key
 
-    print(matching_test_values_sum)
+    return matching_test_values_sum
+
+
+def part_1(data_dict):
+    print(calculate_matching_sum(data_dict, ["+", "*"]))
+
+
+# Gets a bit slower in part two, could be optimized
+def part_2(data_dict):
+    print(calculate_matching_sum(data_dict, ["+", "*", "||"]))
 
 
 file_path = "puzzle_input.txt"
 data_dict = read_and_parse_to_dict(file_path, ":")
 part_1(data_dict)
+part_2(data_dict)
