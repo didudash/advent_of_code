64ec01d6b368c3bed168db4222c7a17c6e880c5c
Diana Rueda <diana.rueda@studium.uni-hamburg.de>
2024-12-17 22:45:27 +0100

    Day 17 part 2 and small refactor part 1

--- 2024/17/main.py
+++ 2024/17/main.py
@@ -7,10 +7,10 @@ def parse_input(file_path):
     return reg_a, reg_b, reg_c, program
 
 
-def part_1(a, b, c, prog):
+def execute_program(a, b, c, program):
     out, ip = [], 0
-    while ip < len(prog):
-        op, operand = prog[ip], prog[ip + 1]
+    while ip < len(program):
+        op, operand = program[ip], program[ip + 1]
         match op:
             case 0:
                 a //= 2 ** (operand if operand < 4 else [a, b, c][operand - 4])
@@ -30,10 +30,33 @@ def part_1(a, b, c, prog):
             case 7:
                 c = a // 2 ** (operand if operand < 4 else [a, b, c][operand - 4])
         ip += 2
-    return ",".join(map(str, out))
+    return out
+
+
+def part_1(a, b, c, program):
+    outputs = execute_program(a, b, c, program)
+    return ",".join(map(str, outputs))
+
+
+def part_2(program, registers_original):
+    valid = [0]
+    for length in range(1, len(program) + 1):
+        oldvalid, valid = valid, []
+        for num in oldvalid:
+            for offset in range(8):
+                newnum = 8 * num + offset
+                outputs = execute_program(
+                    newnum, registers_original["B"], registers_original["C"], program
+                )
+                if outputs == program[-length:]:
+                    valid.append(newnum)
+    return min(valid)
 
 
 choose = {"p": "puzzle", "t": "toy"}.get("p")
 file_path = f"{choose}_input.txt"
-a, b, c, program = parse_input(file_path)
-print(part_1(a, b, c, program))
+reg_a, reg_b, reg_c, program = parse_input(file_path)
+registers_original = {"A": reg_a, "B": reg_b, "C": reg_c}
+
+print(part_1(reg_a, reg_b, reg_c, program))
+print(part_2(program, registers_original))
