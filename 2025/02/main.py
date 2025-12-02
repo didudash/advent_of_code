fc75a7177da2743cffebf9883aa4561709adae5e
Diana Rueda <diana.rueda@studium.uni-hamburg.de>
2025-12-02 09:03:10 +0100

    Add day 02 part 2 and refactor

--- 2025/02/main.py
+++ 2025/02/main.py
@@ -4,23 +4,32 @@ def parse_input(file_path):
         return [tuple(map(int, p.split("-"))) for p in pairs]
 
 
-def part_1(data):
-    invalids = 0
+def iterate_ranges(data):
     for start, end in data:
         for n in range(start, end + 1):
             s = str(n)
             if len(s) > 1 and s[0] == "0":
                 continue
-            length = len(s)
-            if length % 2 == 0:
-                half = length // 2
-                for i in range(half):
-                    if s[i] != s[i + half]:
-                        break
-                else:
-                    invalids += n
-            else:
-                continue
+            yield n, s
+
+
+def part_1(data):
+    invalids = 0
+    for n, s in iterate_ranges(data):
+        if len(s) % 2 == 0:
+            half = len(s) // 2
+            if s[:half] == s[half:]:
+                invalids += n
+    return invalids
+
+
+def part_2(data):
+    invalids = 0
+    for n, s in iterate_ranges(data):
+        for i in range(1, len(s) // 2 + 1):
+            if len(s) % i == 0 and s[:i] * (len(s) // i) == s:
+                invalids += n
+                break
     return invalids
 
 
@@ -30,3 +39,4 @@ file_path = f"{choose}_input.txt"
 data = parse_input(file_path)
 
 print(part_1(data))
+print(part_2(data))
