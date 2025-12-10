519d010ec6f2fcf2f8890476f89cfe9402fb4a8c
didudash <dianaruedade@gmail.com>
2025-12-10 14:43:34 +0100

    Rework day 10 part 1 using combinations

--- 2025/10/main.py
+++ 2025/10/main.py
@@ -1,3 +1,6 @@
+from itertools import combinations
+
+
 def parse_input(file_path):
     with open(file_path, "r") as f:
         return f.read().strip().split("\n")
@@ -11,27 +14,28 @@ def part_1(data):
         target = [c == "#" for c in pattern]
 
         buttons = []
-        rest = line[line.index("]") + 1 :]
-        for part in rest.split("(")[1:]:
+        for part in line.split("(")[1:]:
             if ")" in part:
                 button = [int(x) for x in part[: part.index(")")].split(",")]
                 buttons.append(button)
 
-        min_presses = float("inf")
+        for num_presses in range(len(buttons) + 1):
+            found = False
 
-        for mask in range(1 << len(buttons)):
-            lights = [False] * len(target)
+            for button_combo in combinations(range(len(buttons)), num_presses):
+                lights = [False] * len(target)
 
-            for i, button in enumerate(buttons):
-                if mask & (1 << i):
-                    for light_idx in button:
+                for btn_idx in button_combo:
+                    for light_idx in buttons[btn_idx]:
                         lights[light_idx] = not lights[light_idx]
 
-            if lights == target:
-                presses = bin(mask).count("1")
-                min_presses = min(min_presses, presses)
+                if lights == target:
+                    total += num_presses
+                    found = True
+                    break
 
-        total += min_presses
+            if found:
+                break
 
     return total
 
